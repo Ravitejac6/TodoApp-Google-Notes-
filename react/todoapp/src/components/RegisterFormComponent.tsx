1116716311f7ef140e93fd39f14babf098fceea4
Raviteja Penugonda <raviteja@eazydiner.com>
2021-04-29 00:07:15 +0530

    feat: RegisterForm added
    - Need to add redux
    - Onsubmit user details are getting

--- react/todoapp/src/components/RegisterFormComponent.tsx
+++ react/todoapp/src/components/RegisterFormComponent.tsx
@@ -1,9 +1,50 @@
-import React from "react";
+import { Button, TextField } from "@material-ui/core";
+import React, { ReactNodeArray, useState } from "react";
+import { UserRegisterModel } from "../interfaces/userRegisterModel";
 
 export const RegisterFormComponent = () => {
+  let initialUserRegister: UserRegisterModel = {
+    userName: "",
+    email: "",
+    password: "",
+  };
+  const [userRegister, setUserRegister] = useState<UserRegisterModel>(
+    initialUserRegister
+  );
+  const handleUserRegister = (
+    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    setUserRegister({
+      ...userRegister,
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
   return (
-    <>
-      <h4>Register User</h4>
-    </>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          name="userName"
+          label="UserName"
+          onChange={(e) => handleUserRegister(e)}
+        />
+        <TextField
+          name="email"
+          label="Email"
+          onChange={(e) => handleUserRegister(e)}
+        />
+        <TextField
+          name="password"
+          label="Password"
+          onChange={(e) => handleUserRegister(e)}
+        />
+        <Button variant="contained" color="primary" type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
   );
 };
---
+++ react/todoapp/src/interfaces/userRegisterModel.ts
@@ -0,0 +1,5 @@
+export interface UserRegisterModel {
+  userName: String;
+  email: String;
+  password: String;
+}
